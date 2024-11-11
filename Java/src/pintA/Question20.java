9fc1c90fb7713bceba8907471955d975c14bf6a9
YOKI1216 <1151509140@qq.com>
2024-11-11 21:53:38 +0800

    优化L1-020但仍然超时

--- Java/src/pintA/Question20.java
+++ Java/src/pintA/Question20.java
@@ -3,7 +3,6 @@ package pintA;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -14,46 +13,43 @@ import java.util.HashSet;
  */
 public class Question20 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
-        //定义HashSet集合,将朋友圈中的人放入
+
+        // 使用 HashSet 存储所有有朋友的人
         HashSet<String> friend = new HashSet<>();
+
+        // 处理每个朋友圈
         for (int i = 0; i < n; i++) {
             String[] ids = bf.readLine().split(" ");
-            //没有朋友的人可以是根本没安装“朋友圈”，也可以是只有自己一个人在朋友圈的人
             int k = Integer.parseInt(ids[0]);
-            //如果k=1,即朋友圈中只有一个人,那么他也是没有朋友的,所以当k>1,将这些人的id加入HashSet中
-            if (k != 1) {
+
+            // 如果 k > 1，将这些人加入 friend 集合
+            if (k > 1) {
                 friend.addAll(Arrays.asList(ids).subList(1, ids.length));
             }
         }
-        //输入代查询的人
+
+        // 处理待查询的人的信息
         int m = Integer.parseInt(bf.readLine());
         String[] number = bf.readLine().split(" ");
-        //将没有朋友的人放入Arraylist,并去重
-        ArrayList<String> handsome = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            //没有朋友并且钱没没有查询过这个人
-            if (!friend.contains(number[i]) && !handsome.contains(number[i])) {
-                handsome.add(number[i]);
+
+        // 使用 HashSet 存储没有朋友的人，避免重复输出
+        HashSet<String> handsome = new HashSet<>();
+
+        // 查询每个人是否有朋友
+        for (String person : number) {
+            if (!friend.contains(person)) {
+                handsome.add(person);
             }
         }
-        //没有人帅到没朋友
-        if (handsome.size() == 0) {
+
+        // 如果没有帅到没朋友的人，输出指定信息
+        if (handsome.isEmpty()) {
             System.out.println("No one is handsome");
         } else {
-            //输出帅到没朋友的人
-            for (int i = 0; i < handsome.size(); i++) {
-                System.out.print(handsome.get(i));
-                if (i != handsome.size() - 1) {
-                    System.out.print(" ");
-                }
-            }
+            // 输出帅到没朋友的人的 ID，按照输入顺序输出
+            System.out.println(String.join(" ", handsome));
         }
-
-
     }
 }
-
-
