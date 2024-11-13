a9f030a5ee370f648bb27d4c5346b5fd7381c885
YOKI1216 <1151509140@qq.com>
2024-11-13 10:27:22 +0800

    L1-031 到底是不是太胖了

--- Java/src/pintA/L1_031.java
+++ Java/src/pintA/L1_031.java
@@ -3,19 +3,20 @@ package pintA;
 import java.util.Scanner;
 
 /**
- * @Description :
+ * @Description : 这道题关键点在于输入以及条件判断，别忘了有一个绝对值条件（用Math工具类
  * @Author : YokiWare
- * @Date: 2023/3/14  22:21
+ * @Date: 2024/11/13  22:21
  */
-public class Question31 {
+public class L1_031 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         for (int i = 0; i < N; i++) {
-            int tall = input.nextInt(), weight = input.nextInt() / 2;
-            if (weight - (tall - 100) * 0.9 < (tall - 100) * 0.9 * 0.1) {
+            double tall = input.nextDouble(), weight = input.nextDouble() ;
+            double  standWeight = (tall - 100) * 0.9 *2;
+            if ( Math.abs(weight - standWeight)< standWeight*0.1 ) {
                 System.out.println("You are wan mei!");
-            } else if (weight - (tall - 100) * 0.9 > (tall - 100) * 0.9 * 0.1) {
+            } else if (weight - standWeight > 0) {
                 System.out.println("You are tai pang le!");
             } else System.out.println("You are tai shou le!");
         }
